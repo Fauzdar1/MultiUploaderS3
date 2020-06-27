96b5d73e18e36cb012ceb008cc9bada626dcdebb
Lalit Fauzdar <62703211+Fauzdar1@users.noreply.github.com>
2020-06-27 20:12:15 +0530

    Update MultiDownloaderS3.kt

--- MultiDownloaderS3.kt
+++ MultiDownloaderS3.kt
@@ -17,6 +17,18 @@ import android.content.Context
          }
      }
 
+     private fun s3ClientInitialization(context: Context): AmazonS3 {
+        val cognitoCachingCredentialsProvider = CognitoCachingCredentialsProvider(
+            context,
+            your key,
+            region
+        )
+        return AmazonS3Client(
+            cognitoCachingCredentialsProvider,
+            Region.getRegion(Regions.YOUR_REGION)
+        )
+     }
+
      fun downloadMultiple(fileToKeyDownloads: Map<File, String>, context: Context): Completable? {
          return transferUtility(context)!!
              .flatMapCompletable { transferUtility ->
